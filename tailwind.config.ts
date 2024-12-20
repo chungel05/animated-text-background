832082c44dd73fe32ed0aa6d41c4afadd8ed191e
ChungzZ <chungel05@hotmail.com>
2024-12-19 19:44:44 -0800

    Initial Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,27 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+            animation: {
+                "move-left": "moveLeft 20s linear infinite",
+            },
+            keyframes: {
+                moveLeft: {
+                    "0%": { transform: "translateX(100vw)" },
+                    "100%": { transform: "translateX(-100%)" },
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [require("@designbycode/tailwindcss-text-stroke")],
 } satisfies Config;
